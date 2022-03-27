774104659c5467941d9ddfd0bc081e13a1ffa026
ajreuri23 <shacharkoren23@gmail.com>
2022-03-28 00:32:22 +0300

    removing shit

--- src/utils/ConfigHandler.js
+++ src/utils/ConfigHandler.js
@@ -2,17 +2,20 @@ const smokersGroup = "120363042915528875@g.us";
 const testingGroup = "120363039173872331@g.us";
 
 class ConfigHandler {
-    constructor(client) {
-        this.client = client;
-    }
+  constructor(client) {
+    this.client = client;
+  }
 
-    init() {
-        this.client.chatId = testingGroup;
-        this.client.sendBotMessage = (chatId, message) => {
-            const newMessage = '----------------------------------------\n' + message + '\n----------------------------------------';
-            this.client.sendMessage(chatId, newMessage);
-        }
-    }
+  init() {
+    this.client.chatId = smokersGroup;
+    this.client.sendBotMessage = (chatId, message) => {
+      const newMessage =
+        "----------------------------------------\n" +
+        message +
+        "\n----------------------------------------";
+      this.client.sendMessage(chatId, newMessage);
+    };
+  }
 }
 
 module.exports = ConfigHandler;
