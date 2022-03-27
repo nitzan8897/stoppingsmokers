dd3395dcb9e41a461a6575f45352b6616bed7a8e
ajreuri23 <shacharkoren23@gmail.com>
2022-03-28 00:41:38 +0300

    fixed conflicts

--- src/commands/הנגמלים.js
+++ src/commands/הנגמלים.js
@@ -1,39 +1,32 @@
-const CigaretteReport = require("../models/CigaretteReport");
+const CigaretteReport = require('../models/CigaretteReport')
 
 module.exports.run = async (client, message, args) => {
     try {
-        let message = '';
-        const mentions = [];
-        const topSmokers = await CigaretteReport.aggregate([{ $group: { _id: "$userId", total: { $sum: 1 } } }, { $sort: { total: 1 } }]).exec();
+        let message = ''
+        const mentions = []
+        const topSmokers = await CigaretteReport.aggregate([
+            { $group: { _id: '$userId', total: { $sum: 1 } } },
+            { $sort: { total: -1 } },
+        ]).exec()
         for (const smoker of topSmokers) {
-            smoker._id = await client.getContactById(smoker._id);
+            smoker._id = await client.getContactById(smoker._id)
         }
         topSmokers.slice(0, 5).forEach((smoker, index) => {
-            mentions.push(smoker._id);
-            const user =  `@${smoker._id.id.name}`;
-            const amount = smoker.total;
-            const place = index + 1;
-            message += `במקום ה${place} עם ${amount}, ${user} \n`;
-        });
-        client.sendBotMessage(client.chatId, message, {mentions});
+            mentions.push(smoker._id)
+            const user = `@${smoker._id.id.name}`
+            const amount = smoker.total
+            const place = index + 1
+            message += `במקום ה${place} עם ${amount}, ${user} \n`
+        })
+        client.sendBotMessage(client.chatId, message, { mentions })
     } catch (e) {
-        client.sendBotMessage(client.chatId, 'וואלה לא יודע מה קרה התרחשה שגיאה אחשילי פעם הבאה אולי');
+        client.sendBotMessage(
+            client.chatId,
+            'וואלה לא יודע מה קרה התרחשה שגיאה אחשילי פעם הבאה אולי'
+        )
     }
-    topSmokers.slice(0, 5).forEach((smoker, index) => {
-      const user = smoker._id.pushname;
-      const amount = smoker.total;
-      const place = index + 1;
-      message += `במקום ה${place} עם ${amount}, ${user} \n`;
-    });
-    client.sendBotMessage(client.chatId, message);
-  } catch (e) {
-    client.sendBotMessage(
-      client.chatId,
-      "וואלה לא יודע מה קרה התרחשה שגיאה אחשילי פעם הבאה אולי"
-    );
-  }
-};
+}
 
 module.exports.config = {
-  name: "הנגמלים",
-};
+    name: 'הסרטניים',
+}
