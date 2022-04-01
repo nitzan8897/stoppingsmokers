2be3cf86edb9683b752640d67e0b75359deca1cb
ajreuri23 <shacharkoren23@gmail.com>
2022-04-01 23:12:32 +0300

    Congratulate non smokers for the day

--- src/utils/IntervalMessages.js
+++ src/utils/IntervalMessages.js
@@ -1,4 +1,5 @@
 const warnings = require('../../warnings.json');
+const CigaretteReport = require("../models/CigaretteReport");
 
 class  IntervalMessages {
     #SECONDS_IN_A_DAY = 86400;
@@ -29,7 +30,7 @@ class  IntervalMessages {
         return new Promise((resolve, reject) => {
             setTimeout(() => {
                 resolve();
-            }, timeUntilTomorrow)
+            }, timeUntilTomorrow - 1000 * 20);
         });
     }
 
@@ -37,6 +38,28 @@ class  IntervalMessages {
         const message = 'התחיל יום חדש, אולי היום לא תעשן מה אומר??';
         const warning = warnings.messages[Math.floor(Math.random() * warnings.messages.length)];
         this.client.sendBotMessage(this.client.chatId, `${message}\n קח משהו לזעזע אותך\n ${warning}`);
+        this.#congratulateNonSmokersForToday();
+    }
+
+    async #congratulateNonSmokersForToday() {
+        let message = `קבלו את הילדים הטובים של היום שלא עישנו \n\n`;
+        const contacts= await CigaretteReport.find({}, {userId: 1}).distinct('userId').exec();
+        const mentions = [];
+        for (const contactId of contacts) {
+            const amount = await CigaretteReport.count({
+                userId: contactId,
+                day: new Date().getDay(),
+                month: new Date().getMonth(),
+                year: new Date().getFullYear(),
+            }).exec();
+            if (amount === 0) {
+                const contact = await this.client.getContactById(contactId);
+                message += ` @${contact.id.user}`;
+                mentions.push(amount);
+            }
+        }
+        if (mentions.length === 0) return;
+        this.client.sendBotMessage(this.client.chatId, message, {mentions});
     }
 }
 
