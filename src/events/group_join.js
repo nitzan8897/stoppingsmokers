2b1fa3c73dfde495c85dba4acb51463589ad5d0b
ajreuri23 <shacharkoren23@gmail.com>
2022-04-04 23:25:01 +0300

    added indicative message

--- src/events/group_join.js
+++ src/events/group_join.js
@@ -1,12 +1,14 @@
-const {MessageMedia} = require("whatsapp-web.js");
+const { MessageMedia } = require('whatsapp-web.js')
 
 module.exports = async (client, notification) => {
-    const contacts = await notification.getRecipients();
+    const contacts = await notification.getRecipients()
     for (const contact of contacts) {
-       const contactPictureUrl = await contact.getProfilePicUrl();
-       const contactPicture = await MessageMedia.fromUrl(contactPictureUrl);
-       client.sendBotMessage(client.chatId,
-           `תגידו שלום `,
-           {media: contactPicture, mentions: [contact]})
+        const contactPictureUrl = await contact.getProfilePicUrl()
+        const contactPicture = await MessageMedia.fromUrl(contactPictureUrl)
+        client.sendBotMessage(
+            client.chatId,
+            `תגידו שלום ל@${contact.id.user} שמעתי הוא רוצה להיות ילד טוב, עד עכשיו ספר שיניים \n תעשה !עזרה סמוך \n ואל תשכח לשלוח אמוגי 🚬 למה אני יודע מה עשית בקיץ האחרון🤬`,
+            { media: contactPicture, mentions: [contact] }
+        )
     }
 }
