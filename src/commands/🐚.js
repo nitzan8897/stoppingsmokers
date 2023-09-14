65453fc09586d22f4a56e0837d6aae80c3995f1f
Itay Levi <ItayUriel1@gmail.com>
2023-09-15 02:09:10 +0300

    added magic ball (#14)

---
+++ src/commands/🐚.js
@@ -0,0 +1,28 @@
+const answersMap = require('../../config/magicBall.json')
+const OPEN_SENTENCE = "*קונכיית הקסם אומרת:*\n"
+
+
+module.exports.run = async (client) => {
+
+    const answer = await fetch("https://www.eightballapi.com/api?question=should+I+smoke+this+cigarette?")
+    .then((response) => response.json())
+    .then(answer => {
+        let fAnswer = answer.reading
+        fAnswer = OPEN_SENTENCE + answersMap[fAnswer]
+
+        client.sendMessage(
+            client.chatId,
+            fAnswer
+        );
+    })
+    .catch(error => {
+        console.error(error);
+    });
+
+
+
+};
+
+module.exports.config = {
+    name: "🐚"
+};
