8fb1f8b20ec689bdd9f10c1cf5ba3b4002be3e87
Itay Levi <itayuriel1@gmail.com>
2023-09-15 03:51:19 +0300

    added image to message

--- src/commands/🐚.js
+++ src/commands/🐚.js
@@ -1,14 +1,26 @@
 const magicShellAnswers = require('../../config/magicShell.json')
+const { MessageMedia } = require('whatsapp-web.js')
+
+
 const OPEN_SENTENCE = "*קונכיית הקסם אומרת:*\n"
 
 
 module.exports.run = async (client) => {
 
+    const magicShellGif = MessageMedia.fromFilePath(
+        `./assets/magic-shell.gif`
+    )
+
+
     const answer = magicShellAnswers.answers[(Math.floor(Math.random() * magicShellAnswers.answers.length))]
 
-    client.sendMessage(
+    client.sendBotMessage(
         client.chatId,
-        OPEN_SENTENCE + answer
+        OPEN_SENTENCE + answer,
+        {
+            media: magicShellGif,
+            sendVideoAsGif: true
+        }
     );
 
 };
