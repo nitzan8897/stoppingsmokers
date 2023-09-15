3f4a297e439d1f41680dc01fa6a194272307ace2
Itay Levi <itayuriel1@gmail.com>
2023-09-15 03:06:49 +0300

    changed magicBall to magicShell

--- src/commands/🐚.js
+++ src/commands/🐚.js
@@ -1,25 +1,15 @@
-const answersMap = require('../../config/magicBall.json')
+const magicShellAnswers = require('../../config/magicShell.json')
 const OPEN_SENTENCE = "*קונכיית הקסם אומרת:*\n"
 
 
 module.exports.run = async (client) => {
 
-    const answer = await fetch("https://www.eightballapi.com/api?question=should+I+smoke+this+cigarette?")
-    .then((response) => response.json())
-    .then(answer => {
-        let fAnswer = answer.reading
-        fAnswer = OPEN_SENTENCE + answersMap[fAnswer]
+    const answer = magicShellAnswers.answers[(Math.floor(Math.random() * magicShellAnswers.answers.length))]
 
-        client.sendMessage(
-            client.chatId,
-            fAnswer
-        );
-    })
-    .catch(error => {
-        console.error(error);
-    });
-
-
+    client.sendMessage(
+        client.chatId,
+        OPEN_SENTENCE + answer
+    );
 
 };
 
